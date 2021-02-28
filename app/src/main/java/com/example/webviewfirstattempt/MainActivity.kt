866a10e064dabfbd7010091d344b501185a89c9c
Alexey <alexxd91@gmail.com>
2021-02-28 15:40:29 +0300

    Initial commit

---
+++ app/src/main/java/com/example/webviewfirstattempt/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.webviewfirstattempt
+
+import android.annotation.TargetApi
+import android.app.Activity
+import android.os.Build
+import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    var webView: WebView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        webView = WebView(this)
+        webView!!.settings.javaScriptEnabled = true
+        val activity: Activity = this
+        webView!!.webViewClient = object : WebViewClient() {
+            override fun onReceivedError(
+                view: WebView,
+                errorCode: Int,
+                description: String,
+                failingUrl: String
+            ) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show()
+            }
+
+            @TargetApi(Build.VERSION_CODES.M)
+            override fun onReceivedError(
+                view: WebView,
+                req: WebResourceRequest,
+                rerr: WebResourceError
+            ) {
+                // Redirect to deprecated method, so you can use it in all SDK versions
+                onReceivedError(
+                    view,
+                    rerr.errorCode,
+                    rerr.description.toString(),
+                    req.url.toString()
+                )
+            }
+        }
+
+        //вписать ссылку на сайт (в туториале написано- серверная часть)
+        webView!!.loadUrl("https://fungiforlife.ru/")
+        setContentView(webView)
+    }
+}
